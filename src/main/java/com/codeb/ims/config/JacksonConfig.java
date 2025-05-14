b31bcfdaf41d58aab853545486ce24b0e0ccd40c
l-rohittt-l <sawantdrr@gmail.com>
2025-05-15 00:22:14 +0530

    Fix: migrate to jackson-datatype-hibernate6 to resolve javax.persistence.Transient error

--- src/main/java/com/codeb/ims/config/JacksonConfig.java
+++ src/main/java/com/codeb/ims/config/JacksonConfig.java
@@ -1,17 +1,20 @@
 package com.codeb.ims.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+
 @Configuration
 public class JacksonConfig {
-
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new Hibernate5Module());
+        mapper.registerModule(new Hibernate6Module());
         return mapper;
     }
 }
